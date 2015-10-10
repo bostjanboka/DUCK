e043393da277fdbccaedc3186b75b356acc8329d
bostjanboka <bohtebostjan@gmail.com>
2015-10-10 22:31:23 +0200

    jašek, samo še reset rabi

---
+++ DUCK UNDER/Assets/JasekSkripta.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JasekSkripta : MonoBehaviour {
+
+    // Use this for initialization
+    int stEnter;
+    GameObject coliderji;
+    public GameObject[] racka;
+	void Start () {
+        stEnter = 0;
+        coliderji = transform.FindChild("COLIDERS").gameObject;
+        coliderji.SetActive(false);
+        racka = new GameObject[16];
+        Transform crna = transform.FindChild("CRNA");
+        for(int i=0; i < racka.Length; i++)
+        {
+            racka[i] = crna.GetChild(i).GetChild(0).gameObject;
+            racka[i].SetActive(false);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(stEnter == 0)
+        {
+            coliderji.SetActive(true);
+        }
+        stEnter++;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        stEnter--;
+        if(stEnter == 0)
+        {
+            coliderji.SetActive(false);
+        }
+    }
+}
---
+++ DUCK UNDER/Assets/ResetkaSkripta.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetkaSkripta : MonoBehaviour {
+
+    // Use this for initialization
+    
+    int indeks;
+    void Start () {
+ 
+        indeks = int.Parse("" + gameObject.name[gameObject.name.Length - 1]);
+
+        if (gameObject.name[gameObject.name.Length - 2] != '_')
+        {
+            indeks += 10;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (transform.parent.parent.GetComponent<JasekSkripta>().racka[indeks].activeSelf && other.gameObject.CompareTag("otrok"))
+        {
+            other.gameObject.SetActive(false);
+            RackaSkripta.stRack--;
+            transform.parent.parent.GetComponent<JasekSkripta>().racka[indeks].SetActive(true);
+        }
+    }
+}
