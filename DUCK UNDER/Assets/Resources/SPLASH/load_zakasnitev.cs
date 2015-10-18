5459aa4ad43c3e7524afa2e05e5ef7e874119f5b
kraljo <aljosa@mordenkul.com>
2015-10-18 02:31:20 +0200

    SPLASH update

---
+++ DUCK UNDER/Assets/Resources/SPLASH/load_zakasnitev.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class load_zakasnitev : MonoBehaviour {
+
+    public GameObject LOADING;
+
+    private void Start()
+    {
+        StartCoroutine(ActivationRoutine());
+    }
+
+    private IEnumerator ActivationRoutine()
+    {
+        yield return new WaitForSeconds(1.25f);
+        LOADING.SetActive(true);
+    }
+}
